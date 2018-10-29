609715aa1c1b744cdb7c4bd7b55c1ef5712f7c5c
caoyu5779 <caoyu5779@126.com>
2018-10-29 11:53:42 +0800

    代码框架提交

---
+++ api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+
+	router.POST("/user", CreateUser)
+	router.POST("/user/:user_name", Login)
+
+	return router
+}
+
+func main()  {
+	//每个goroutine 只有4k大小？
+	r := RegisterHandlers()
+	http.ListenAndServe(":8111", r)
+
+
+}
+
+//handler->validation{1: request , 2. user} -> business logic -> response
+// 1. data model
+// 2. error handling
